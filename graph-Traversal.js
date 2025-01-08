25fb2738882b5f5703c0e1ca1127a6501ed686c7
Jonathan Giraldo Mosquera <gitboi56@outlook.com>
2025-01-08 22:53:52 +0000

    notes

--- graph-Traversal.js
+++ graph-Traversal.js
@@ -1,2 +1,8 @@
 // visiting/ updating / checking each vertex in a graph 
+// we need to state our starting point 
+
+// depth first traversal first follow the children before the 
+// siblings
+
+/*  */
 
