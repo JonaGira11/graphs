0e1bcb14c5a7d8bb44940b2fdbd817cf27cbe8f1
Jonathan Giraldo Mosquera <gitboi56@outlook.com>
2025-01-22 13:37:36 +0000

    weighted graph

--- Dijkstatra's.js
+++ Dijkstatra's.js
@@ -2,7 +2,13 @@ class WeightedGraph {
     constructor() {
         this.adjencyList = {};
     }
-    
+    addVertex(vertex){
+        if(!this.adjencyList[vertex]) this.adjencyList[vertex] = [];
+    }
+    addEdge(vertex1,vertex2,weight){
+        this.adjencyList[vertex1].push({node:vertex2, weight});
+        this.adjencyList[vertex2].push({node:vertex1, weight});
+    }
 }
 
 {
